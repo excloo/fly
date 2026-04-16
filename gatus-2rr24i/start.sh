6e517ee40480fe52dfa8a6cbae79cbb9b81f9666
Max Schaefer <maxexcloo@users.noreply.github.com>
2026-04-16 17:31:01 +1000

    Update gatus-2rr24i start.sh

--- gatus-2rr24i/start.sh
+++ gatus-2rr24i/start.sh
@@ -2,5 +2,5 @@
 set -e
 export PATH="/app:$PATH"
 tailscaled --socket=/var/run/tailscale/tailscaled.sock --state=/var/lib/tailscale/tailscaled.state &
-tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=gatus
+tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=gatus &
 GATUS_CONFIG_PATH=/app/config gatus
