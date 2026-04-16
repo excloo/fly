ee0c12ad173bb270cf7e7178dcc9c0a3d80181b9
Max Schaefer <maxexcloo@users.noreply.github.com>
2026-04-16 17:02:23 +1000

    Update gatus-fly start.sh

--- gatus-2rr24i/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-/app/tailscaled --socket=/var/run/tailscale/tailscaled.sock --state=/var/lib/tailscale/tailscaled.state &
-/app/tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=gatus
-GATUS_CONFIG_PATH=/app/config /app/gatus
