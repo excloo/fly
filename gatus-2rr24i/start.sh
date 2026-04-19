82d6ea014c105f24a8c03b45eea6728030699ed9
Max Schaefer <maxexcloo@users.noreply.github.com>
2026-04-19 16:23:54 +1000

    Update gatus-fly start.sh

---
+++ gatus-2rr24i/start.sh
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:3Qm1PvW0+cvOdzQShNHdJDCIDN7j0h+xx1RcTBt9Gy3vAlQ4hvyPpLrormuQ36EdCJycdjHoqhOHmbS0IZK9/O8QJXHOY+8QDTVe1bUlyR4pWPlItsxB4x/nhwRnldBM/g6FsehErZHPe9g5ij3qP8gstz50+zB3IXFG2CGzCw14E4q+6ET8Xq9F5H3VFRv8N7EbSYaLJakb2hIuY+bZ+mqcECaUZtR4fBk7HI99N1LeeXb/+7G6O6HiRm2hUVYNWisoPLNa6SU1nU1ci9+RtuiNo1Jro8saymIB//TRCZkXhNfacRnoBZdZV2BnZsopSHo=,iv:KS6aE6Z/jnkiIBK6n5oQV/WCIwPJ8Ze4FiFKd/p61Sc=,tag:Oz4i1lRwwd3dfm28W1gQUA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1cnktwwj4lq4lm5qa3vxn2ft5n38h557smr2f58t9vz4zum5w0udqzzle6c",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBlOUdTQTZmeSsvRTZ3WTZo\nZS8ybGFJZzVwSFU2MnJ1Zlg4amdickZGMlJ3Cnd5MlB6WXNVRDNoakdpamN4d0xR\ndHJqc0JHYUowcWRabDlqc1dLOG9KUGsKLS0tIHI0d0VOdXJnZlJWQndmRHN6M3ZI\nNkVLRjNqc1EvWW9uVTIwVlBsZ2h1T2MKjVxrGg4gS1o5PtiEkX1Bvvoia5NNKnev\nLUBts51hdZZOay+IgGyWJyXo7GmaLr1reRDUrNPpps3mXvFz5SaDJQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-04-19T06:23:38Z",
+		"mac": "ENC[AES256_GCM,data:pJX27N1tl6b2d5of/qUGkDfT+cEI2CyPIphrh3D08FA+pu74PBDp8lDv7AmJ8A7U9RW/Jb9M11arkycq1WDn9rxOwqNuNHl1pFttkEkmextUjRD4K0rCXz69XkTrJVQ50r8Aq7bWH3iZMAT2SLjOBtEaoOJnLGxuDnvqmMQSdC8=,iv:+HRqo4sd7ma45BXj4DX0pEsHMtxXqA6nPobnncDapps=,tag:eMfKM4dZwDQ3+95HxJjv1g==,type:str]",
+		"version": "3.12.1"
+	}
+}
